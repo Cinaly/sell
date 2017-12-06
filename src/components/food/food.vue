a3573d995f39f3406ee002506823f4ab30604670
Cinaly <834518573@qq.com>
2017-12-06 21:02:17 +0800

    0

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -185,4 +185,11 @@
         padding:0 8px
         font-size: 12px
         color: rgb(77,85,93)
+    .rating
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 6px
+        font-size: 14px
+        color:rgb(7,17,27)
 </style>
