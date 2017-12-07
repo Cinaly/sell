198945ce8e6a831f56b32a773fc53887940fded4
Cinaly <834518573@qq.com>
2017-12-07 21:01:00 +0800

    0

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,11 +1,95 @@
 <template>
-<div>i am ratings</div>
+  <div class="ratings">
+    <div class="ratings-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">{{seller.score}}</h1>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="deliveryTime">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default{}
+  import star from '../star/star'
+
+  export default{
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    components: {
+      star
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .ratings
+    position: absolute
+    top: 174px
+    bottom: 0
+    left: 0
+    width: 100%
+    overflow: hidden
+    .overview
+      display: flex
+      padding: 18px 0
+      .overview-left
+        padding :6px 0
+        flex: 0 0 137px
+        width: 137px
+        text-align :center
+        border-right: 1px solid rgba(7, 17, 27, 0.1)
+        .score
+          font-size :24px
+          line-height: 28px
+          color:rgb(255,153,0)
+          margin-bottom: 6px
+        .title
+          font-size: 12px
+          line-height 12px
+          color:rgb(7,17,27)
+          margin-bottom :8px
+        .rank
+          font-size :10px
+          line-height :10px
+          color: rgb(147,153,159)
+      .overview-right
+        flex: 1
+        .score-wrapper
+          line-height: 18px
+          font-size: 12px
+          margin-bottom: 8px
+          font-size :0
+          .title
+            font-size :12px
+            color: rgb(7,17,27)
+          .star
+            display :inline-block
+            margin:0 12px
+            vertical-align :top
+          .score
+            color: rgb(255,153,0)
+            vertical-align :top
+            font-size :12px
 </style>
--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,5 +1,24 @@
 <template>
-
+  <div class="ratingselect">
+    <div class="rating-type">
+      <span @click="select(2,$event)" class="block positive" :class="{active:selectType_r===2}">
+        {{desc.all}}
+        <span class="count">{{ratings.length}}</span>
+      </span>
+      <span @click="select(0,$event)" class="block positive" :class="{active:selectType_r===0}">
+        {{desc.positive}}
+        <span class="count">{{positives.length}}</span>
+      </span>
+      <span @click="select(1,$event)" class="block negative" :class="{active:selectType_r===1}">
+        {{desc.negative}}
+        <span class="count">{{negatives.length}}</span>
+      </span>
+    </div>
+    <div @click="toggleContent" class="switch">
+      <span class="icon-check_circle" :class="{on:onlyContent_r}"></span>
+      <span class="text">只看有内容的评价</span>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -31,10 +50,89 @@
           negative: '不满意'
         }
       }
+    },
+    data() {
+      return {
+        onlyContent_r: this.onlyContent,
+        selectType_r: this.selectType
+      }
+    },
+    computed: {
+      positives() {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === POSITIVE
+        })
+      },
+      negatives() {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === NEGATIVE
+        })
+      }
+    },
+    methods: {
+      select(type, event) {
+        if (!event._constructed) {
+          return
+        }
+        this.selectType_r = type
+        this.$emit('ratingtype-select', this.selectType_r)
+      },
+      toggleContent(event) {
+        if (!event._constructed) {
+          return
+        }
+        this.onlyContent_r = !this.onlyContent_r
+        this.$emit('content-toggle', this.onlyContent_r)
+      }
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
+  .ratingselect
+    .rating-type
+      padding: 18px 0
+      margin: 0 18px
+      border-1px(rgba(7, 17, 27, 0.1))
+      font-size: 0
+      .block
+        display: inline-block
+        padding: 8px 12px
+        margin-right: 8px
+        color: #ffffff
+        border-radius: 1px
+        font-size: 12px
+        color: rgb(77, 85, 93)
+        line-height: 16px
+        &.active
+          color: #fff
+        .count
+          margin-left: 2px
+          font-size: 8px
+        &.positive
+          background: rgba(0, 160, 220, 0.2)
+          &.active
+            background: rgb(0, 160, 220)
+        &.negative
+          background: rgba(77, 85, 93, 0.2)
+          &.active
+            background: rgb(77, 85, 93)
+    .switch
+      padding: 12px 18px
+      line-height: 24px
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+      color: rgb(147, 153, 159)
+      font-size: 0
+      .icon-check_circle
+        margin-right: 4px
+        font-size: 24px
+        display: inline-block
+        vertical-align: top
+        &.on
+          color: #00c850
+      .text
+        font-size: 12px
 
 </style>
